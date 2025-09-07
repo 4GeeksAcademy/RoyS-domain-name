d1ac72ecf05871eef1be329a50b3dd025a604b6e
Roy S. <bookingrstubbs@gmail.com>
2025-09-07 16:28:46 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -1,21 +1,27 @@
 import "bootstrap";
 import "./style.css";
+window.onload = function(){
+
+let pronounList = ["my", "your", "the", "our"];
+let adjList = ["amazing", "crazy", "funny", "cool"];
+let nounList = ["developer", "pizza", "unicorn", "geek"];
+let extList = [".com", ".net", ".us", ".int", ".dev"];
+
+const domainsArea = document.querySelector("#domains");
 
 
-let pronoun = ['my', 'your', 'the', 'our'];
-let adj = ['amazing', 'crazy', 'funny', 'cool'];
-let noun = ['developer', 'pizza', 'unicorn', 'geek'];
-let extensions = ['.com', '.net', '.us', '.int', '.dev'];
+for (let p of pronounList) {
+  for (let a of adjList) {
+    for (let n of nounList) {
+      for (let ext of extList) {
+        const domain = document.createElement("p")
+        domain.textContent = p + a + n + ext
+        console.log(p + a + n + ext);
+        domainsArea.appendChild(domain)
+      }
+    }
+  }
+}
 
-    for (let p of pronounList) {
-        for (let a of adjList) {
-              for (let n of nounList) {
-                      for (let ext of extList) {
-
-                                console.log(p + a + n + ext);
-                                        }
-                                              }
-                                                  }
-                                                    }
-                                                    }
-                                                    return domains;
+return;
+}
